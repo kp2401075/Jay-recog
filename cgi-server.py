47e9dd2411df6f2c8308739cd14778ea4ed86b50
kp2401075 <kp2401075@gmail.com>
2016-08-27 17:41:49 +1000

    Add files via upload

---
+++ cgi-server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+ 
+import BaseHTTPServer
+import CGIHTTPServer
+import cgitb; cgitb.enable()  ## This line enables CGI error reporting
+import os
+
+
+
+ 
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+server_address = ("", 8000)
+handler.cgi_directories = ["/"]
+ 
+httpd = server(server_address, handler)
+httpd.serve_forever()
